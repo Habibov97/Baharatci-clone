039317a0bed844c83809b431f2ed236aa488d610
Habibov97 <najaff.habibov@gmail.com>
2024-12-17 11:00:31 +0400

    small fix for loader

--- src/components/ProductsMain.jsx
+++ src/components/ProductsMain.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
 
 
 function ProductsMain({ data }) {
