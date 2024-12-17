eb580094710e8c7c851fef36abab21d2bef00dc1
Habibov97 <najaff.habibov@gmail.com>
2024-12-17 10:39:37 +0400

    fixing main images

--- src/components/ProductsMain.jsx
+++ src/components/ProductsMain.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import images from '../assets/img/mehsullar';
+
 
 function ProductsMain({ data }) {
     const [visibleCount, setVisibleCount] = useState(14);
     const navigate = useNavigate();
+
+    const images = import.meta.glob('/src/assets/img/mehsullar/*.{png,jpg,jpeg,webp}');
+
+    // Convert imported images into an accessible object
+    const imagePaths = {};
+    for (const path in images) {
+        const fileName = path.split('/').pop(); // Extract the file name
+        imagePaths[fileName] = images[path];
+    }
+
 
     const visibleData = data.flatMap(item => item.cards).slice(0, visibleCount);
     const hasMore = visibleCount < data.flatMap((item) => item.cards).length;
@@ -19,7 +29,7 @@ function ProductsMain({ data }) {
                 <div key={i}  className="card-product">
                   <div className='card-product-img'>
                     <img
-                      src={`${images}/${card.img}`}
+                      src={imagePaths[card.img]}
                       alt={card.name}
                     />
                   </div>
