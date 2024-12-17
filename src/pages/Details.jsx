d5e4a7496cee5c7aaac5f076606159350d1a6645
Habibov97 <najaff.habibov@gmail.com>
2024-12-17 19:22:10 +0400

    fixed Details.jsx img show

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { BaharatContext } from '../App';
-import '../assets/css/pages.css';
-import { FaWhatsapp } from 'react-icons/fa';
+import '../assets/css/pages.css'
+import { FaWhatsapp } from "react-icons/fa";
 import SimilarProducts from '../components/SimilarProducts';
-import { useDispatch } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { addToBasket } from '../slices/basketSlice';
 
 //Toaster
@@ -16,37 +16,23 @@ function Details() {
   const [imagePaths, setImagePaths] = useState({});
   const { id } = useParams();
   const data = useContext(BaharatContext);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const allProducts = data.flatMap((item) => item.cards);
-  const product = allProducts.find((item) => item.id == id);
+  const allProducts = data.flatMap(item => item.cards);
+  const product = allProducts.find(item => item.id == id);
+
 
   if (!product) {
-    return (
-      <div
-        style={{
-          width: '100%',
-          height: '50vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}
-      >
-        <p style={{ fontSize: '2em', fontFamily: 'cursive', padding: '25px' }}>
-          Məhsul tapılmadı.
+    return <div style={{width:'100%', height:'50vh', display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
+        <p style={{fontSize:'2em', fontFamily:'cursive', padding:'25px'}}>
+            Məhsul tapılmadı.   
         </p>
-        <Link to="/" style={{ fontSize: '1.3em', fontFamily: 'cursive' }}>
-          Ana səhifəyə dön
-        </Link>
-      </div>
-    );
+        <Link to='/' style={{fontSize:'1.3em', fontFamily:'cursive',}}>Ana səhifəyə dön</Link> 
+    </div>
   }
 
   useEffect(() => {
-    const images = import.meta.glob(
-      '/src/assets/img/mehsullar/*.{png,jpg,jpeg,webp}'
-    );
+    const images = import.meta.glob('/src/assets/img/mehsullar/*.{png,jpg,jpeg,webp}');
 
     const loadImages = async () => {
       const resolvedImages = {};
@@ -61,70 +47,59 @@ function Details() {
     loadImages();
   }, []);
 
-  const handleAddToBasket = () => {
-    dispatch(addToBasket(product));
-    notify();
-  };
+  const handleAddToBasket = ()=> {
+    dispatch(addToBasket(product))
+    notify()
+  }
   // toast mesaji
-  const notify = () =>
-    toast.success('Məhsul səbətə əlavə olundu!', {
-      position: 'bottom-right',
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: 'light',
-      transition: Flip,
+  const notify = () => toast.success('Məhsul səbətə əlavə olundu!',{
+    position: "bottom-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Flip,
     });
 
   return (
-    <>
-      <div style={{ background: '#eee' }}>
-        <div className="haqqimizdaBg">
-          <h2 className="mehsul-haqqinda">Məhsul Haqqında</h2>
-        </div>
+    <> 
+      <div style={{background:'#eee'}}>
+          <div className='haqqimizdaBg'>
+                  <h2 className='mehsul-haqqinda'>Məhsul Haqqında</h2>   
+          </div>
       </div>
 
       <div className="details-container">
-        <div className="details-image">
-          {imagePaths[item.img] ? (
-            <img src={imagePaths[item.img]} alt={item.name} />
+          <div className="details-image">
+          {imagePaths[product.img] ? (
+            <img src={imagePaths[product.img]} alt={product.name} />
           ) : (
             <p>Loading...</p>
           )}
-        </div>
-        <div className="details-content">
-          <h1>{product.name}</h1>
-          <p className="price">{product.price}</p>
-          <ul className="details-list">
-            <li>{product.about}</li>
-            <li>100% Təbii</li>
-            <li>Müştərinin istəyindən asılı həcm (whatsapp)</li>
-          </ul>
-          <div className="order-btns">
-            <button
-              className="whatsapp-button toBasket-btn"
-              onClick={handleAddToBasket}
-            >
-              Səbətə at
-            </button>
-            <button
-              className="whatsapp-button"
-              onClick={() =>
-                window.open('https://wa.me/994702549600', '_blank')
-              }
-            >
-              Whatsapp ilə sifariş{' '}
-              <FaWhatsapp className="wp-icon" style={{ marginLeft: '10px' }} />
-            </button>
           </div>
-        </div>
+          <div className="details-content">
+            <h1>{product.name}</h1>
+            <p className="price">{product.price}</p>
+            <ul className="details-list">
+              <li>{product.about}</li>
+              <li>100%  Təbii</li>
+              <li>Müştərinin istəyindən asılı həcm (whatsapp)</li>
+            </ul>
+            <div className='order-btns'>
+              <button className="whatsapp-button toBasket-btn" onClick={handleAddToBasket}>Səbətə at</button>
+              <button className="whatsapp-button" onClick={() => window.open('https://wa.me/994702549600', '_blank')}>Whatsapp ilə sifariş <FaWhatsapp className='wp-icon' style={{marginLeft : '10px' ,}} /></button>
+            </div>
+            
+          </div>
       </div>
 
-      <SimilarProducts data={data} product={product} />
+      <SimilarProducts data={data} product={product}/>
+    
     </>
+    
   );
 }
 
